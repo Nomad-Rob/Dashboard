942d0b1a7cef5c9dc9cf41fdc22048ab065b5d49
Nomad-Rob <rfarley89@gmail.com>
2024-03-20 19:36:40 -0500

    syntax fix. Setup ThemeProvider for all app

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { themeSettings } from 'theme';
+
 
 
 function App() {
+  // Get the current mode from the store
+  const mode = useSelector((state) => state.mode);
+  // Create a theme with the current mode
+  const theme = useMemo(() => createTheme(themeSettings[mode]), [mode]);
   return (
     <div className="app">
+      {/* Apply the theme to the entire app easy setup*/}
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
     </div>
   );
 }
